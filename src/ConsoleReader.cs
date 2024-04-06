618c86a385b6b9c5dea44e271519ff39517edf5e
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-04-06 13:52:35 -0400

    The handler is subscribed once.

---
+++ src/ConsoleReader.cs
@@ -0,0 +1,56 @@
+﻿namespace MultipleEventSubscriptions
+{
+    internal class ConsoleReader
+    {
+        private const string PROMPT = "> ";
+        private const string STR_LOWER_Q = "q";
+        private const string STR_UPPER_Q = "Q";
+
+        public delegate void EventConsoleInput(string pInput);
+        public event EventConsoleInput? consoleInput;
+
+        private bool _isActive = false;
+
+        public void Start()
+        {
+            if(_isActive)
+            {
+                return;
+            }
+
+            _isActive = true;
+
+            while(_isActive)
+            {
+                Console.Write(PROMPT);
+                string? input = Console.ReadLine();
+
+                if (input == null)
+                {
+                    Stop();
+                    break;
+                }
+
+                if (input == STR_LOWER_Q || input == STR_UPPER_Q)
+                {
+                    Stop();
+                }
+
+                TriggerConsoleInput(input);
+            }
+        }
+
+        public void Stop()
+        {
+            _isActive = false;
+        }
+
+        private void TriggerConsoleInput(string pInput)
+        {
+            if (consoleInput != null)
+            {
+                consoleInput(pInput);
+            }
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MultipleEventSubscriptions
+{
+    internal class Program
+    {
+        private static string NEW_LINE = "\n";
+
+        static void Main(string[] args)
+        {
+            ConsoleReader reader = new ConsoleReader();
+            reader.consoleInput += ProcessConsoleInput;
+            reader.Start();
+        }
+
+        private static void ProcessConsoleInput(string pInput)
+        {
+            Console.WriteLine(pInput + NEW_LINE);
+        }
+    }
+}
