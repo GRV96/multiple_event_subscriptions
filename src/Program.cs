175197183727d336b9ba9bb1068effe9e4d943a1
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-04-06 14:09:52 -0400

    The argument specifies the number of subscriptions.

--- src/Program.cs
+++ src/Program.cs
@@ -6,10 +6,27 @@
 
         static void Main(string[] args)
         {
+            int nbSubscriptions = 1;
+            if(args.Length >= 1)
+            {
+                Int32.TryParse(args[0], out nbSubscriptions);
+            }
+
             ConsoleReader reader = new ConsoleReader();
-            reader.consoleInput += ProcessConsoleInput;
-            //reader.consoleInput += ProcessConsoleInput;
+
+            for(int i=0; i<nbSubscriptions; i++)
+            {
+                reader.consoleInput += ProcessConsoleInput;
+            }
+
             reader.Start();
+        }
+
+        private static int ParseNbSubscriptions(string pArg)
+        {
+            int nbSubscriptions = 1;
+
+            return nbSubscriptions;
         }
 
         private static void ProcessConsoleInput(string pInput)
